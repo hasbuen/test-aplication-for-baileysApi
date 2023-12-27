83e3002f508af2e70f6cfc3dcfb481af4bbc7729
Julio Cesar Ovídio Bueno <juliocesar.ovidiobueno@outlook.com>
2023-12-27 14:29:22 -0300

    creatin a new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import TextCard from "."
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <div className="container">
+
+        <div className="row">
+
+          <TextCard />
+          
+        </div>
+
+      </div>
+
     </>
   )
 }
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+function TextCard() {
+    
+    return(
+        <h1>{import.meta.env.VITE_TOKEN}</h1>
+    );
+};
+
+export default TextCard;
